55196029a068ef3ae4dd564c7aa8b001bb9cafa5
Muhammad Ahsan <muhammadahsanty@gmail.com>
2023-09-05 01:05:30 -0700

    Voice Assistant Project Complete

--- controller/device_State_Controller.js
+++ controller/device_State_Controller.js
@@ -53,7 +53,6 @@ exports.create = async (req, res, next) => {
     });
 };
 
-
 exports.update = async (req, res, next) => {
     const deviceList = req.body.list; // Get the list of devices from the request
 
@@ -61,44 +60,93 @@ exports.update = async (req, res, next) => {
     const updatedDeviceStates = [];
 
     for (const device of deviceList) {
-        const model = {
-            device: device.device,
-            property: device.properties[0].property,
-            value: device.properties[0].value,
-            type: device.properties[0].type,
-            last_updated_ms: device.last_updated_ms,
-        };
+        for (const property of device.properties) {
+            const model = {
+                device: device.device,
+                property: property.property,
+                value: property.value,
+                type: property.type,
+                last_updated_ms: device.last_updated_ms,
+            };
 
-        // try {
-        //     await deviceStateSchema.validateAsync(model, { abortEarly: false });
-        // } catch (error) {
-        //     const errorMessages = error.details.map(detail => detail.message);
-        //     return res.status(422).json({ errors: errorMessages });
-        // }
+            // try {
+            //     await deviceStateSchema.validateAsync(model, { abortEarly: false });
+            // } catch (error) {
+            //     const errorMessages = error.details.map(detail => detail.message);
+            //     return res.status(422).json({ errors: errorMessages });
+            // }
 
-        deviceStateService.updateDeviceState(model, (error, result) => {
-            if (error) {
-                console.error("Error in Updating DeviceState:", error);
-                return res.status(500).json({ error: "Error in Updating DeviceState", message: error.message });
-            } else {
-                updatedDeviceStates.push(result);
+            deviceStateService.updateDeviceState(model, (error, result) => {
+                if (error) {
+                    console.error("Error in Updating DeviceState:", error);
+                    return res.status(500).json({ error: "Error in Updating DeviceState", message: error.message });
+                } else {
+                    updatedDeviceStates.push(result);
 
-                if (updatedDeviceStates.length === deviceList.length) {
-                    // All devices have been updated, send the response
-                    const response = {
-                        timestamp_ms: Date.now(),
-                        action: "Update",
-                        List: updatedDeviceStates,
-                    };
+                    if (updatedDeviceStates.length === deviceList.length * device.properties.length) {
+                        // All devices and properties have been updated, send the response
+                        const response = {
+                            timestamp_ms: Date.now(),
+                            action: "Update",
+                            List: updatedDeviceStates,
+                        };
 
-                    const jsonResponse = JSONBig.stringify(response);
+                        const jsonResponse = JSONBig.stringify(response);
 
-                    return res.status(200).json(JSONBig.parse(jsonResponse)); // Parse it back to JSON
+                        return res.status(200).json(JSONBig.parse(jsonResponse)); // Parse it back to JSON
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 };
+
+
+// exports.update = async (req, res, next) => {
+//     const deviceList = req.body.list; // Get the list of devices from the request
+
+//     // Iterate through the list of devices and update each DeviceState
+//     const updatedDeviceStates = [];
+
+//     for (const device of deviceList) {
+//         const model = {
+//             device: device.device,
+//             property: device.properties[0].property,
+//             value: device.properties[0].value,
+//             type: device.properties[0].type,
+//             last_updated_ms: device.last_updated_ms,
+//         };
+
+//         // try {
+//         //     await deviceStateSchema.validateAsync(model, { abortEarly: false });
+//         // } catch (error) {
+//         //     const errorMessages = error.details.map(detail => detail.message);
+//         //     return res.status(422).json({ errors: errorMessages });
+//         // }
+
+//         deviceStateService.updateDeviceState(model, (error, result) => {
+//             if (error) {
+//                 console.error("Error in Updating DeviceState:", error);
+//                 return res.status(500).json({ error: "Error in Updating DeviceState", message: error.message });
+//             } else {
+//                 updatedDeviceStates.push(result);
+
+//                 if (updatedDeviceStates.length === deviceList.length) {
+//                     // All devices have been updated, send the response
+//                     const response = {
+//                         timestamp_ms: Date.now(),
+//                         action: "Update",
+//                         List: updatedDeviceStates,
+//                     };
+
+//                     const jsonResponse = JSONBig.stringify(response);
+
+//                     return res.status(200).json(JSONBig.parse(jsonResponse)); // Parse it back to JSON
+//                 }
+//             }
+//         });
+//     }
+// };
 
 
 
